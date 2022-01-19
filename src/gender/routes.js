40999f7d676ac3f96fbe5ff29be1ce44b75d644c
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2022-01-19 00:39:31 -0400

    Changing route exportation

--- src/gender/routes.js
+++ src/gender/routes.js
@@ -2,12 +2,10 @@ const route = require('express').Router();
 const validator = require('./validator');
 const controller = require('./controller');
 
-const routes = () => {
-  route.get('/genders', controller.list);
-  route.get('/gender/:id', validator.isValidId, controller.getOne); // TO DO: return gender info by id
-  route.post('/gender', validator.create, controller.create);
-  route.delete('/gender/:id', validator.isValidId, controller.remove);
-  route.patch('/gender/:id', validator.isValidId, controller.update);
-};
+route.get('/genders', controller.list);
+route.get('/gender/:id', validator.isValidId, controller.getOne); // TO DO: return gender info by id
+route.post('/gender', validator.create, controller.create);
+route.delete('/gender/:id', validator.isValidId, controller.remove);
+route.patch('/gender/:id', validator.isValidId, controller.update);
 
-module.exports = routes;
+module.exports = route;
