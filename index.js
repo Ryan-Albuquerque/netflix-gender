07e17deb3265504d244676dd60f95e6fef3a30a2
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2022-01-19 00:52:33 -0400

    Listen port before initialization of pkgs

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const routesConfig = require('./routesConfig');
 
 const app = express();
 
+app.listen('3000', async () => {
+  await db.connect();
+  console.log('API is running in 3000!');
+});
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
 app.use(routesConfig);
-
-app.listen('3000', async () => {
-  await db.connect();
-  console.log('API is running in 3000!');
-});
